4670c15eed9a4854a19093813d6ba3f2b15f3af8
Marie245 <marie.queiros@aluno.ce.gov.br>
2023-08-30 16:27:37 -0300

    request api

--- project/src/pages/Feed/index.jsx
+++ project/src/pages/Feed/index.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import { Card } from '../../components/Card'
 import './styles.css'
+import api from '../../lib/axios'
 
 export function Feed() {
+
+  const [post, setPost] = useState([])
+
+  useEffect(() => {
+    api.get('/posts')
+      .then((response) => {
+        console.log(response.data)
+        setPost(response.data)
+      }).catch(() => {
+        console.log("Error: 404")
+      })
+  }, [])
+
   return (
     <div className="feedContainer">
-      <Card />
+      {
+        post.map((posts) => {
+          <Card post={post.post}/>
+        })
+      }
     </div>
   );
 }
